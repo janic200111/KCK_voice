332784e04b224e5c8454ca121e57c31db6f05f70
lewuss <zenek.zupa@gmail.com>
2023-01-06 18:58:05 +0100

    Qol Changes

--- inf148240_inf148136.py
+++ inf148240_inf148136.py
@@ -1,34 +1,47 @@
 from __future__ import division
-from matplotlib import pylab as plt
 import numpy as np
 import scipy.io.wavfile
-from scipy.sig import kaiser, decimate
-from copy import copy
-import os, re
+from scipy.signal import kaiser, decimate
+from copy import deepcopy
+import os
 import sys
 
+
+def absolute_file_paths(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
+
+
 def analyze_wav(filename):
-  sampling_rate, sig = scipy.io.wavfile.read(filename)
-  smp_cnt = len(sig)
-  duration = float(smp_cnt) / sampling_rate
-  if not isinstance(sig[0], np.int16):
-    sig = [s[0] for s in sig]
-  sig = sig * kaiser(smp_cnt, 100)
+    sampling_rate, sig = scipy.io.wavfile.read(filename)
+    smp_cnt = len(sig)
+    duration = float(smp_cnt) / sampling_rate
+    if not isinstance(sig[0], np.int16):
+        sig = [s[0] for s in sig]
+    sig = sig * kaiser(smp_cnt, 100)
 
-  spc = np.log(abs(np.fft.rfft(sig)))
-  hps = copy(spc)
-  for h in np.arange(2, 6):
-    dec = decimate(spc, h)
-    hps[:len(dec)] += dec
-  peak_start = 50 * duration
-  peak = np.argmax(hps[int(peak_start):])
-  verdict = (peak_start + peak) / duration
+    spc = np.log(abs(np.fft.rfft(sig)))
+    hps = deepcopy(spc)
+    for h in range(2, 6):
+        dec = decimate(spc, h)
+        hps[:len(dec)] += dec
+    peak_start = 50 * int(duration)
+    peak = np.argmax(hps[peak_start:])
 
-  return 'M' if verdict < 165 else 'K'
+    return 'M' if (peak_start + peak) / duration < 165 else 'K'
 
+
+# recognized = 0
+# directory = "trainall"
+# files = absolute_file_paths(directory)
+# for file in files:
+#     if analyze_wav(file) == file[-5]:
+#         recognized += 1
+# print(recognized)
 
 try:
-  filename=os.path.basename(sys.argv[1])
-  print(analyze_wav(filename))
+    filename = os.path.basename(sys.argv[1])
+    print(analyze_wav(filename))
 except:
     print("K")
